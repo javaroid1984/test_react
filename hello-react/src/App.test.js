a65c477d9a0071a1bfaf413ee0d0074e28c30d4e
권상혁 <glassdama@gwonsanghyeog-ui-MacBook-Pro.local>
2019-01-06 02:12:58 +0900

    변경2,3

--- hello-react/src/App.test.js
+++ hello-react/src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+//변경2
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+//변경3
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
